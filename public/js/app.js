d2837c0f134f0f7af0185e4c7a44d1e876b6847d
Taoufik Fekih <tafekih@outlook.fr>
2021-03-22 11:30:08 +0100

    updated composer

--- public/js/app.js
+++ public/js/app.js
@@ -55679,8 +55679,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/todoapp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/todoapp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\git\test\laravue-todoapp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\git\test\laravue-todoapp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
